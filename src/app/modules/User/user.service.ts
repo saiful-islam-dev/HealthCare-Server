7323d34cec54f4dfb504ca49028a9476b683fe32
Saiful264 <saifulislamx264@gmail.com>
2024-03-17 16:05:33 +0600

    Add a api

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,10 +1,34 @@
+import { PrismaClient, UserRole } from "@prisma/client"
+import * as bcrypt from 'bcrypt'
 
-const createAdmin = async(data: any)=>{
-    console.log(data);
-return {
-    Meassage: "this is sercie user" 
-} 
-}
+const prisma = new PrismaClient();
+
+const createAdmin = async (data: any) => {
+    const hashedPassword = await bcrypt.hash(data.password, 12)
+
+
+    const userData = {
+        email: data.admin.email,
+        password: hashedPassword,
+        role: UserRole.ADMIN,
+    };
+    console.log(userData);
+
+  const result = await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.user.create({
+      data: userData,
+    });
+
+    const createdAdminData = await transactionClient.admin.create({
+      data: data.admin,
+    });
+
+    return createdAdminData;
+  });
+  console.log(result);
+  return result;
+};
+
 
 export const userServeice = {
     createAdmin
