56e3222024b3110d7660fe0da3a20a2239901e7c
Saiful264 <saifulislamx264@gmail.com>
2024-03-26 23:43:18 +0600

    Frongot password api was completed

--- src/app/modules/Auth/auth.routes.ts
+++ src/app/modules/Auth/auth.routes.ts
@@ -3,13 +3,20 @@ import { AuthController } from "./auth.controller";
 import auth from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
 
-const route = express.Router();
+const router = express.Router();
 
 
-route.post("/login", AuthController.loginUser);
+router.post("/login", AuthController.loginUser);
 
-route.post("/refresh-token", AuthController.refreshToken);
+router.post("/refresh-token", AuthController.refreshToken);
 
-route.post("/change-password", auth(UserRole.ADMIN, UserRole.DOCTOR, UserRole.SUPER_ADMIN, UserRole.PATIENT), AuthController.changePassword);
+router.post("/change-password", 
+auth(UserRole.ADMIN, UserRole.DOCTOR, UserRole.SUPER_ADMIN, UserRole.PATIENT),
+ AuthController.changePassword
+);
 
-export const AuthRouters = route;
+router.post("/forgot-password", AuthController.forgotPassword);
+
+router.post("/reset-password", AuthController.resetPassword);
+
+export const AuthRouters = router; 
--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -4,7 +4,9 @@ import { jwtHelpers } from "../../../helper/jwtHelpers";
 import { UserStatus } from "@prisma/client";
 import config from "../../../config";
 import { Secret } from "jsonwebtoken";
-import { date } from "zod";
+import emailSender from "./emailSender";
+import ApiError from "../../errors/ApiError";
+import httpStatus from "http-status";
 
 const loginUser = async (payload: {
     email: string,
@@ -114,8 +116,77 @@ const changePassword = async(user: any, payload: any)=>{
 
 }
 
+const forgotPassword = async(payload: {email: string}) =>{
+  const userData = await prisma.user.findUniqueOrThrow({
+    where:{
+      email: payload.email,
+      status: UserStatus.ACTIVE
+    }
+  })
+
+
+  const resetPassToken = jwtHelpers.generateToken(
+    {email: userData.email, role: userData.role},
+    config.jwt.reset_pass_secret as Secret,
+    config.jwt.reset_pass_token_expires_in as string
+  )
+
+  const resetPassLink = config.reset_pass_link + `?userId=${userData.id}&token=${resetPassToken}`;
+
+  emailSender(
+    userData.email,
+    ` <div>
+    <p>Dear User,</p>
+    <p>Your password reset link 
+        <a href=${resetPassLink}>
+            <button>
+                Reset Password
+            </button>
+        </a>
+    </p>
+
+</div>`)
+}
+
+const resetPassword = async (token: string, payload: { id: string, password: string })=>{
+  console.log({token}, payload);
+
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: {
+        id: payload.id,
+        status: UserStatus.ACTIVE
+    }
+});
+
+console.log({userData});
+
+const isValidToken = jwtHelpers.verifyToken(token, config.jwt.reset_pass_secret as Secret)
+
+if (!isValidToken) {
+    throw new ApiError(httpStatus.FORBIDDEN, "Forbidden!")
+}
+
+// hash password
+const password = await bcrypt.hash(payload.password, 12);
+console.log(password);
+
+// update into database
+const result = await prisma.user.update({
+    where: {
+        id: payload.id
+    },
+    data: {
+        password
+    }
+})
+console.log({result});
+}
+
+
 export const AuthServices = {
     loginUser,
     refreshToken,
-    changePassword
+    changePassword,
+    forgotPassword,
+    resetPassword
 }
