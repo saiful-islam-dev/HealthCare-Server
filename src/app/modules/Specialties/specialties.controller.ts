f43092ba4b78b982b3577f23eadd1110ee0693ea
Saiful264 <saifulislamx264@gmail.com>
2024-04-14 22:03:15 +0600

    Patient work done

--- src/app/modules/Specialties/specialties.controller.ts
+++ src/app/modules/Specialties/specialties.controller.ts
@@ -3,7 +3,6 @@ import catchAsync from "../../../Share/catchAsync";
 import sendResponse from "../../../Share/sendResponse";
 import { Request, Response } from "express";
 import { SpecialtiesService } from "./specialties.services";
-
 const inserIntoDB = catchAsync(async (req: Request, res: Response) => {
   console.log(req.body);
   const result = await SpecialtiesService.inserIntoDB(req);
@@ -18,7 +17,6 @@ const inserIntoDB = catchAsync(async (req: Request, res: Response) => {
 
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
   const result = await SpecialtiesService.getAllFromDB();
-
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
@@ -27,4 +25,19 @@ const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
-export const SpecialtiesController = { getAllFromDB, inserIntoDB };
+const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await SpecialtiesService.deleteFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Specialty deleted successfully",
+    data: result,
+  });
+});
+
+export const SpecialtiesController = {
+  getAllFromDB,
+  inserIntoDB,
+  deleteFromDB,
+};
--- src/app/modules/Specialties/specialties.services.ts
+++ src/app/modules/Specialties/specialties.services.ts
@@ -1,9 +1,11 @@
 import { Request } from "express";
 import prisma from "../../../Share/prisma";
 import { fileUploader } from "../../../helper/fileUploader";
+import { Specialties } from "@prisma/client";
+import { IFile } from "../../interface/file";
 
 const inserIntoDB = async (req: Request) => {
-  const file = req.file;
+  const file = req.file as IFile;
 
   if (file) {
     const uploadToCloudinary = await fileUploader.uploadTocloudinary(file);
@@ -17,12 +19,20 @@ const inserIntoDB = async (req: Request) => {
   return result;
 };
 
-const getAllFromDB = async () => {
-  console.log("object");
+const getAllFromDB = async (): Promise<Specialties[]> => {
+  return await prisma.specialties.findMany();
 };
 
+const deleteFromDB = async (id: string): Promise<Specialties> => {
+  const result = await prisma.specialties.delete({
+    where: {
+      id,
+    },
+  });
+  return result;
+};
 export const SpecialtiesService = {
   inserIntoDB,
   getAllFromDB,
-  // deleteFromDB
+  deleteFromDB,
 };
