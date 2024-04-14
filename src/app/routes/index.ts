f88934e1e0fecbcc3b1e96e00e9c314a4084cda6
Saiful264 <saifulislamx264@gmail.com>
2024-04-14 22:10:24 +0600

    Patient work is done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,30 +1,35 @@
-import express from 'express';
-import { AdminRoutes } from '../modules/Admin/admin.routes';
-import { UserRoutes } from '../modules/User/user.routes';
-import { AuthRouters } from '../modules/Auth/auth.routes';
-import { SpecialtiesRouters } from '../modules/Specialties/specialties.routes';
+import express from "express";
+import { AdminRoutes } from "../modules/Admin/admin.routes";
+import { UserRoutes } from "../modules/User/user.routes";
+import { AuthRouters } from "../modules/Auth/auth.routes";
+import { SpecialtiesRoutes } from "../modules/Specialties/specialties.routes";
+import { PatientRoutes } from "../modules/Patient/patient.routes";
 
 const router = express.Router();
 
 const moduleRoutes = [
-    {
-        path: '/user',
-        route: UserRoutes
-    },
-    {
-        path: '/admin',
-        route: AdminRoutes
-    },
-    {
-        path: '/auth',
-        route: AuthRouters
-    },
-    {
-        path: '/specialties',
-        route: SpecialtiesRouters
-    }
+  {
+    path: "/user",
+    route: UserRoutes,
+  },
+  {
+    path: "/admin",
+    route: AdminRoutes,
+  },
+  {
+    path: "/auth",
+    route: AuthRouters,
+  },
+  {
+    path: "/specialties",
+    route: SpecialtiesRoutes,
+  },
+  {
+    path: "/patient",
+    route: PatientRoutes,
+  },
 ];
 
-moduleRoutes.forEach(route => router.use(route.path, route.route))
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
